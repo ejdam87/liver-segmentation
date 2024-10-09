e1969f3797fa7857430dbf40ada9f03b3a9aad9e
ejdam87 <adam.dzadon@gmail.com>
2024-10-09 09:23:32 +0300

    Dataset implementation

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,1 +1,35 @@
-# TODO
+import torch
+import numpy as np
+import torchvision.transforms as transforms
+from numpy.typing import NDArray
+from PIL import Image
+
+
+def get_image(path: str) -> torch.tensor:
+    """
+    Returns image located at <path> as tensor
+    """
+    im = Image.open(path)
+    transform = transforms.ToTensor()
+    return transform(im)
+
+
+class ImageDataset(torch.utils.data.Dataset):
+
+    def __init__(self, X: NDArray, Y: NDArray) -> None:
+        super().__init__()
+        assert len(X) == len(Y), "The number of inputs and expected outputs (labels) must be equal"
+        self.X = X
+        self.Y = Y
+
+    def __len__(self) -> int:
+        """
+        Returns number of samples
+        """
+        return len(self.X)
+
+    def __getitem__(self, i: int) -> tuple[ torch.Tensor, torch.Tensor ]:
+        """
+        Returns i-th sample as a pair of input and expected output
+        """
+        return get_image( self.X[i] ), get_image( self.Y[i] )
