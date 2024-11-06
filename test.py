5590a5a58f3f9897bee22ba2b9b03c9297b7dbb6
ejdam87 <adam.dzadon@gmail.com>
2024-11-06 12:58:21 +0200

    Testing progress bar add

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import torch
 import torch.utils
 import torch.utils.data
 import torchmetrics
+from tqdm import tqdm
+
 
 def test_model(model: torch.nn.Module,
                test_data: torch.utils.data.DataLoader,
@@ -19,15 +21,16 @@ def test_model(model: torch.nn.Module,
     """
 
     print("Starting testing...")
-    with torch.no_grad():   # no need of gradient for validation
+    with torch.no_grad():
         metric_values = {name: 0 for name in test_metrics}
         n_batches = len(test_data)
+        test_progress_bar = tqdm(test_data, desc="Test progress")
 
-        for X_batch, Y_batch in test_data:
+        for X_batch, Y_batch in test_progress_bar:
             pred = model(X_batch)
 
             for name, metric in test_metrics.items():
-                metric_values[name] += metric( pred, Y_batch )
+                metric_values[name] += metric( pred, Y_batch ).item()
 
         for name, val in metric_values.items():
             print( f"{name} = {val / n_batches : .3f}" )
