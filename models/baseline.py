0e148a1b2dd8dbe8a5782ba4e6a421c5f841c115
ejdam87 <adam.dzadon@gmail.com>
2024-11-18 13:20:55 +0200

    Add baseline

--- models/baseline.py
+++ models/baseline.py
@@ -1,1 +1,25 @@
-# TODO
+import torch
+
+class baseline_4(torch.nn.Module):
+    """
+    Baseline model
+    """
+
+    def __init__(self, in_channels: int, out_channels: int) -> None:
+        """
+        in_channels : How many channels does the input image have (1 - grayscale, 3 - rgb)
+        out_channels : How many channels does the output image have (same meaning as <in_channels>)
+        """
+        super(baseline_4, self).__init__()
+        self.conv = torch.nn.Conv2d(in_channels, 64, kernel_size=3, padding="same")
+        self.relu = torch.nn.ReLU(inplace=True)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.upsample = torch.nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.output = torch.nn.Conv2d(64, out_channels, kernel_size=1)
+        self.out_activation = torch.nn.Sigmoid()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.pool(self.relu(self.conv(x)))
+        x = self.upsample(x)
+        x = self.output(x)
+        return self.out_activation(x)
